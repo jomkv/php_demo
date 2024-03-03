dcfd5eba05091f987a27aad756c3b35296df3ea8
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-03-03 18:06:24 +0800

    challenge activity for the 3 previous modules

--- _starter_files/06_functions.php
+++ _starter_files/06_functions.php
@@ -53,3 +53,67 @@
   }
 
   // test_function();
+
+  // * !! Challenge activity !!
+  // * Create a PHP script that calculates and displays the grades of students
+  // * based on their scores. The script should use conditionals, arrays, and loops.
+
+  // A: 90-100
+  // B: 80-89
+  // C: 70-79
+  // D: 60-69
+  // F: 0-59
+
+  function determineGrade($gradeNum) {
+    if($gradeNum >= 90) {
+      return 'A';
+    }
+    else if($gradeNum >= 80) {
+      return 'B';
+    }
+    else if($gradeNum >= 70) {
+      return 'C';
+    }
+    else if($gradeNum >= 60) {
+      return 'D';
+    }
+    else {
+      return 'F';
+    }
+  }
+
+  $studentGrades = [
+    'John' => 79, //  C
+    'Bro' => 86, // B
+    'Jom' => 69, // D 
+    'Einstein' => 100, // A
+    'Bob' => 55 // F
+  ];
+
+  $highest = key($studentGrades);
+  $lowest = key($studentGrades);
+  $classAvg = 0;
+
+  foreach($studentGrades as $studentName => $studentGrade) {
+
+    // get class avg 
+    $classAvg += $studentGrade;
+
+    // get class lowest and highest
+    if ($studentGrade > $studentGrades[$highest]) {$highest = $studentName;}
+    if ($studentGrade < $studentGrades[$lowest]) {$lowest = $studentName;}
+
+    // print student's grade
+    $grade = determineGrade($studentGrade);
+    echo $studentName . ' - ' . $grade . '<br>';
+  }
+
+  $classAvg /= count($studentGrades);
+
+  echo 'Class Average Grade: ' . $classAvg . '<br>';
+  echo 'Best Student: ' . $highest . '<br>';
+  echo 'Worst Student: ' . $lowest . '<br>';
+
+  
+
+
