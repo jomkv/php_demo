99fef00c755b6691ed5565858e5206ac6c5ca2a9
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-03-03 16:22:28 +0800

    module 6 done

--- _starter_files/06_functions.php
+++ _starter_files/06_functions.php
@@ -12,3 +12,44 @@
       // code to be executed
     }
   */
+
+  // function createUser($username, $email) {
+  //   echo 'New user! <br>';
+  //   echo 'Username: ' . $username . '<br>';
+  //   echo 'Email: ' . $email . '<br>';
+  // }
+
+  // createUser('jomarculus', 'jomarculus@hehe.com');
+
+  // * Functions with default values
+
+  function getSum($n1 = 1, $n2 = 1) {
+    return $n1 + $n2;
+  }
+
+  // $res = getSum(2, 2);
+  // echo $res;
+
+  // * Anonymous functions
+
+  $subtract = function($n1, $n2) {
+    return $n1 - $n2;
+  };
+
+  // echo $subtract(10, 5);
+
+  // * Shorthand return
+
+  $multiply = fn($n1, $n2) => $n1 * $n2;
+
+  // echo $multiply(3, 3);
+
+  // * Accessing global variables within local scopes
+  $test_var = 'hihi';
+
+  function test_function() {
+    GLOBAL $test_var;
+    echo $test_var;
+  }
+
+  // test_function();
