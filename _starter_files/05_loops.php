697b5db35e9a67e8383f2a574ae2507fe17ddb36
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-03-03 16:11:25 +0800

    module 5 done

--- _starter_files/05_loops.php
+++ _starter_files/05_loops.php
@@ -1,45 +1,91 @@
 <?php
 
-/* -------- Loops & Iteration ------- */
+  /* -------- Loops & Iteration ------- */
 
-/* ------------ For Loop ------------ */
+  /* ------------ For Loop ------------ */
 
-/*
-** For Loop Syntax
-  for (initialize; condition; increment) {
-  // code to be executed
-  }
-*/
+  /* 
+  ** For Loop Syntax
+    for (initialize; condition; increment) {
+    // code to be executed 
+    }
+  */
+
+  // for($i = 2; $i <= 10; $i += 2) {
+  //   echo $i . '<br>';
+  // }
+
+  /* ------------ While Loop ------------ */
+
+  /*
+  ** While Loop Syntax
+    while (condition) {
+    // code to be executed
+    }
+  */
+
+  // $count = 1;
+  // while($count <= 5) {
+
+  //   if($count === 3) {
+  //     $count++;
+  //     continue;
+  //   }
+
+  //   echo $count . '<br>';
+
+  //   $count++;
+    
+  // }
 
 
-/* ------------ While Loop ------------ */
+  /* ---------- Do While Loop --------- */
 
-/*
-** While Loop Syntax
-  while (condition) {
-  // code to be executed
-  }
-*/
+  /*
+  ** Do While Loop Syntax
+    do {
+    // code to be executed
+    } while (condition);
 
+  do...while loop will always execute the block of code once, even if the condition is false.
+  */
 
-/* ---------- Do While Loop --------- */
+  // $i = 1;
 
-/*
-** Do While Loop Syntax
-  do {
-  // code to be executed
-  } while (condition);
+  // do {
+  //   echo 'Number ' . $i . '<br>';
+  //   $i++;
+  // } while($i < 0);
+  
+  /* ---------- Foreach Loop ---------- */
 
-do...while loop will always execute the block of code once, even if the condition is false.
-*/
+  /*
+  ** Foreach Loop Syntax
+    foreach ($array as $value) {
+    // code to be executed
+    }
+  */
 
+  $posts = ['Post 1', 'Post 2', 'Post 3', 'FINAL POST'];
 
+  // for($i = 0; $i < count($posts); $i++) {
+  //   echo $posts[$i] . '<br>';
+  // }
 
-/* ---------- Foreach Loop ---------- */
+  // foreach($posts as $post) {
+  //   echo $post . '<br>';
+  // }
 
-/*
-** Foreach Loop Syntax
-  foreach ($array as $value) {
-  // code to be executed
-  }
-*/
+  // foreach($posts as $index => $post) {
+  //   echo $index . ' - ' . $post . '<br>'; 
+  // }
+
+  // $students = [
+  //   'FiRST INDEX' => 'hello i am student 1',
+  //   'SECAND INDEFX' => 'hello i am the 2nd student',
+  //   'LAST STUDENT' => 'last but not the least student' 
+  // ];
+
+  // foreach($students as $key => $student) {
+  //   echo $key . ' - ' . $student . '<br>';
+  // }
