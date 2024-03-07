4505e20970b56fb4781b0fd8e44ea3c8453808bc
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-03-07 16:57:50 +0800

    Module 9 done

--- _starter_files/09_superglobals.php
+++ _starter_files/09_superglobals.php
@@ -15,4 +15,29 @@
     $_FILES -  Contains information about files uploaded to the script.
     $_REQUEST - Contains information about variables passed through the form or URL.
   */
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <ul>
+    <li>Host: <?php echo $_SERVER['HTTP_HOST']; ?></li>
+    <li>Document Root: <?php echo $_SERVER['DOCUMENT_ROOT']; ?></li>
+    <li>System Root: <?php echo $_SERVER['SystemRoot']; ?></li>
+    <li>Server Name: <?php echo $_SERVER['SERVER_NAME']; ?></li>
+    <li>Server Port: <?php echo $_SERVER['SERVER_PORT']; ?></li>
+    <li>Current File Dir: <?php echo $_SERVER['PHP_SELF']; ?></li>
+    <li>Request URI: <?php echo $_SERVER['REQUEST_URI']; ?></li>
+    <li>Server Software: <?php echo $_SERVER['SERVER_SOFTWARE']; ?></li>
+    <li>Client Info: <?php echo $_SERVER['HTTP_USER_AGENT']; ?></li>
+    <li>Remote Address: <?php echo $_SERVER['REMOTE_ADDR']; ?></li>
+    <li>Remote Port: <?php echo $_SERVER['REMOTE_PORT']; ?></li>
+  </ul>
+</body>
+</html>
