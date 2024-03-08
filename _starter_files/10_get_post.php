23f505d3efba6a4594c3dd7395113dafae1b66b6
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-03-08 14:16:22 +0800

    module 10, 11 done

--- _starter_files/10_get_post.php
+++ _starter_files/10_get_post.php
@@ -4,3 +4,21 @@
   /*
     We can pass data through urls and forms using the $_GET and $_POST superglobals.
   */
+
+  if(isset($_POST['submit'])) {
+    echo $_POST["name"];
+    echo $_POST["age"];
+  }
+?>
+
+<form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
+  <div>
+    <label for="name">Enter name</label>
+    <input type="text" name="name" />
+  </div>
+  <div>
+    <label for="age">Enter Age</label>
+    <input type="text" name="age" />
+  </div>
+  <button type="submit" name="submit">Submit</button>
+</form>
--- _starter_files/11_sanitizing_inputs.php
+++ _starter_files/11_sanitizing_inputs.php
@@ -4,3 +4,29 @@
   /*
     Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
   */
+
+
+  // * isset(var) - returns bool if var is null or not
+  if(isset($_POST["submit"])) {
+    echo htmlspecialchars($_POST["name"]);
+    echo htmlspecialchars($_POST["age"]);
+  }
+
+  // * more direct way
+  // if($_POST["name"] && $_POST["name"]) {
+  //   echo $_POST["name"];
+  //   echo $_POST["age"];
+  // }
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+  <div>
+    <label for="name">Enter name</label>
+    <input type="text" name="name" />
+  </div>
+  <div>
+    <label for="age">Enter Age</label>
+    <input type="text" name="age" />
+  </div>
+  <button type="submit" name="submit">Submit</button>
+</form>
