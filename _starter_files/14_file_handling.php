c63f4c01acc752246654edfd2120c618736f1b4f
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-03-09 16:23:58 +0800

    module 14 done

--- _starter_files/14_file_handling.php
+++ _starter_files/14_file_handling.php
@@ -1,8 +1,39 @@
 <?php
 
-  /* ---------- File Handling --------- */
+/* ---------- File Handling --------- */
 
-  /* 
+/* 
     File handling is the ability to read and write files on the server.
     PHP has built in functions for reading and writing files.
   */
+
+$file = 'extras/users.txt';
+
+// * file_exists(file) - checks if file is properly loaded 
+if (file_exists($file)) {
+
+  // * readfile(file) - outputs the contents of the file
+  // echo readfile($file);
+
+  // * fopen(file, mode) - returns a pointer to the file being opened
+  // * - can do different actions depending on the mode
+  $handle = fopen($file, 'r');
+
+  // * fread(pointer to file, length / file size) - outputs the content
+  // * - of the file into the variable being set to
+  $contents = fread($handle, filesize($file));
+
+  // * fclose(pointer) - self explanatory, good practice after opening a file
+  fclose($handle);
+  echo $contents;
+} else {
+  $handle = fopen($file, 'w');
+
+  // * PHP_EOL - line break for text files
+  $contents = 'Jom' . PHP_EOL . 'Jomarculus';
+
+  // * fwrite(pointer, content) - writes the content into the pointer's file
+  fwrite($handle, $contents);
+
+  fclose($handle);
+}
