b7a322f044909e58156bde39d7c4b87cbfecef85
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-03-17 15:04:57 +0800

    feat: fetch feedback from db

--- _starter_files/feedback/feedback.php
+++ _starter_files/feedback/feedback.php
@@ -1,32 +1,28 @@
 <?php include 'components/header.php'; ?>
 
+<?php
+$query = "SELECT * FROM feedback";
+$res = mysqli_query($conn, $query);
+$feedbacks = mysqli_fetch_all($res, MYSQLI_ASSOC); // format as associative array
+?>
+
 <h2>Feedback</h2>
 
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-    molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-    repellendus voluptatibus natus deserunt sed doloribus inventore,
-    totam labore maxime perferendis!
-  </div>
-</div>
-
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-    molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-    repellendus voluptatibus natus deserunt sed doloribus inventore,
-    totam labore maxime perferendis!
-  </div>
-</div>
-
-<div class="card my-3">
-  <div class="card-body">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-    molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-    repellendus voluptatibus natus deserunt sed doloribus inventore,
-    totam labore maxime perferendis!
-  </div>
-</div>
+<?php if (empty($feedbacks)) : ?>
+  <p>no feedbacks</p>
+<?php else : ?>
+  <?php foreach ($feedbacks as $item) : ?>
+    <div class="card my-3 w-75">
+      <div class="card-body text-center">
+        <?php echo $item["body"]; ?>
+        <div class="text-secondary mt-2">
+          By <?php echo $item["name"]; ?>
+          <br>
+          on <?php echo $item["date"]; ?>
+        </div>
+      </div>
+    </div>
+  <?php endforeach ?>
+<?php endif ?>
 
 <?php include 'components/footer.php'; ?>
